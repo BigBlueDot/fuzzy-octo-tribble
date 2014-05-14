d7c2e27cb8790af033626ecc46a9f9cccef11c86
BigBlueDot <BigBlueDotGit@gmail.com>
2014-05-13 22:11:11 -0500

    fixed bad stuttering of map movement
    fixes #400

--- fuzzy-octo-tribble/Scripts/Game/Camera.js
+++ fuzzy-octo-tribble/Scripts/Game/Camera.js
@@ -69,12 +69,14 @@
         var width = $('.game-map').width();
 
         //tx and ty represent the offset of tiles being displayed based loosely on window size
-        tx = cx % squareSize;
-        ty = cy % squareSize;
+        tx = Math.abs(cx % squareSize);
+        ty = Math.abs(cy % squareSize);
 
         //MapIndexX and MapIndexY are the indices of the top left most visible block
         MapIndexX = Math.floor(cx / squareSize);
         MapIndexY = Math.floor(cy / squareSize);
+
+        console.log("tx: " + tx + " ty: " + ty + " MapIndexX: " + MapIndexX + " MapIndexY: " + MapIndexY);
 
         //MapWidth and MapHeight are the width and height using "Map Squares" as the unit
         MapWidth = (Math.floor((width + tx) / squareSize)) + 3;
@@ -165,8 +167,8 @@
             for (var x = startX; x < maxX; x++) {
                 for (var y = startY; y < maxY; y++) {
                     drawSquare(map.mapUrl[map.mapSquares[x][y].i],
-                        (x * squareSize) - cx + tx,
-                        (y * squareSize) - cy + ty);
+                        (x * squareSize) - cx - tx,
+                        (y * squareSize) - cy - ty);
                 }
             }
 
@@ -183,16 +185,16 @@
 
     var drawPlayer = function () {
         if (!FuzzyOctoTribble.PlayerDirection || FuzzyOctoTribble.PlayerDirection === 4) {
-            drawSquare("/Images/Game/Map/PlayerDown.png", px - cx + tx, py - cy + ty, "player-square");
+            drawSquare("/Images/Game/Map/PlayerDown.png", px - cx - tx, py - cy - ty, "player-square");
         }
         else if (FuzzyOctoTribble.PlayerDirection === 1) {
-            drawSquare("/Images/Game/Map/PlayerLeft.png", px - cx + tx, py - cy + ty, "player-square");
+            drawSquare("/Images/Game/Map/PlayerLeft.png", px - cx - tx, py - cy - ty, "player-square");
         }
         else if (FuzzyOctoTribble.PlayerDirection === 2) {
-            drawSquare("/Images/Game/Map/PlayerUp.png", px - cx + tx, py - cy + ty, "player-square");
+            drawSquare("/Images/Game/Map/PlayerUp.png", px - cx - tx, py - cy - ty, "player-square");
         }
         else if (FuzzyOctoTribble.PlayerDirection === 3) {
-            drawSquare("/Images/Game/Map/PlayerRight.png", px - cx + tx, py - cy + ty, "player-square");
+            drawSquare("/Images/Game/Map/PlayerRight.png", px - cx - tx, py - cy - ty, "player-square");
         }
     }
 
